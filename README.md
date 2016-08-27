8f41e093b587b74fc8652e071d603a27accc0172
Allen Chou <cjcat2266@gmail.com>
2016-08-27 00:10:46 -0700

    Add quaternion-spring.

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ Here are the source files used to test algorithms and generate example animation
 * [Numeric Springing Examples](http://allenchou.net/2015/04/game-math-numeric-springing-examples/)
 * [More on Numeric Springing](http://allenchou.net/2015/04/game-math-more-on-numeric-springing/)
 
-*Ming-Lun "Allen" Chou*  
+![box-springs](http://allenchou.net/wp-content/uploads/2015/04/spring.gif)
+![button-spring](http://allenchou.net/wp-content/uploads/2015/04/button-spring.gif)
+![angular-spring](http://allenchou.net/wp-content/uploads/2015/04/angular-spring.gif)
+![animation-spring](http://allenchou.net/wp-content/uploads/2015/04/animation-spring.gif)
+
+----
+**Ming-Lun "Allen" Chou**  
 [AllenChou.net](http://AllenChou.net)  
 [@TheAllenChou](http://twitter.com/TheAllenChou)  
---
+++ quaternion-spring/Assets/QuaternionSpring.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuaternionSpring : MonoBehaviour {
+
+	float x, v, xt;
+	Vector3 dir;
+
+	// Use this for initialization
+	void Start () {
+		x = 0.0f;
+		v = 0.0f;
+		xt = 0.0f;
+	}
+
+	float mx, my;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 m = Input.mousePosition;
+			mx = m.x;
+			my = m.y;
+		}
+
+		if (Input.GetMouseButton(0)) {
+			Vector3 m = Input.mousePosition;
+			float dx = m.x - mx;
+			float dy = m.y - my;
+			float r = Mathf.Sqrt (dx * dx + dy * dy);
+			if (r > 1.0f) {
+				x = r;
+				dir.Set (dx, 0.0f, dy);
+			}
+		} else {
+			Spring (ref x, ref v, xt, 0.23f, 8.0f * Mathf.PI, Time.deltaTime);
+		}
+		transform.rotation = Quaternion.AngleAxis (x, Vector3.Cross (Vector3.up, dir));
+	}
+
+	void Spring  
+		(  
+		 ref float x, ref float v, float xt, 
+		 float zeta, float omega, float h  
+		 )  
+	{  
+		float f = 1.0f + 2.0f * h * zeta * omega;  
+		float oo = omega * omega;  
+		float hoo = h * oo;  
+		float hhoo = h * hoo;  
+		float detInv = 1.0f / (f + hhoo);  
+		float detX = f * x + h * v + hhoo * xt;  
+		float detV = v + hoo * (xt - x);  
+		x = detX * detInv;  
+		v = detV * detInv;  
+	}   
+}
